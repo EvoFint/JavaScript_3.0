842d277ef76938e9b9eea17a42401aa934d089b2
Roman <evofint@gmail.com>
2018-08-14 00:52:37 +0700

    ls4 hard v02

--- lesson 4/task/exam/index.js
+++ lesson 4/task/exam/index.js
@@ -1,34 +1,45 @@
-let finish = [];
-let start = 1;
-let end = 300;
 function getFriendlyNumbers(start, end) {
-    for(let i = start; i < (end - 1); i++) {
-        sum1 = summNumber(start);
-        sum2 = summNumber(sum1);
-        if(sum2 == start && sum1 != start && sum1 > start) {
-            writeInArr(sum1, sum2);
+    let finish = [];
+    for(let i = start; i < end; i++) {
+        sum1 = getDivisorsSum(i);
+        sum2 = getDivisorsSum(sum1);
+        if(sum2 == i && sum1 != sum2 && sum1 > start) {
+            let c = writeInArr(sum1, sum2);
+            finish = finish.push(c);
         }
     }
-    return [];
+    return finish;
 }
-function summNumber(num) {
+console.log(getFriendlyNumbers(1, 300));
+function getSumm(arr) {
     let sum = 0;
-    for(let  i = 1; i < (num - 1); i++) {
-        if(num % i == 0) {
-            sum = sum + i;
-        }
+    for(let i = 0; i < arr.length; i++) {
+        sum += arr[i];
     }
     return sum;
 }
+function getDivisors(num) {
+    let arr = [];
+    for(let i = 1; i < num; i++) {
+        if(num % i == 0) {
+            arr.push(i);
+        }
+    }
+    return arr;
+}
+function getDivisorsSum(num) {
+    return getSumm(getDivisors(num));
+}
 function writeInArr(a, b) {
     let c = a + ', ' + b;
-    finish.push(c);
+    return c;
 }
 
-getFriendlyNumbers(start, end);
 
 module.exports = {
     firstName: 'Name',
     secondName: 'Surname',
     task: getFriendlyNumbers
 }
+
+//getFriendlyNumbers(1, 300);
