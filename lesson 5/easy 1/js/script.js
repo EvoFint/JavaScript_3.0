7927f9e90af9f4149d1555978a90206d1113edfc
Roman <evofint@gmail.com>
2018-08-17 00:48:15 +0700

    ls5 -fix "fiveMenu"

--- lesson 5/easy 1/js/script.js
+++ lesson 5/easy 1/js/script.js
@@ -2,13 +2,17 @@ let menu = document.querySelector('.menu');
 let li2 = document.querySelectorAll('.menu-item')[1];
 let li3 = document.querySelectorAll('.menu-item')[2];
 let title = document.querySelector('.title');
-let column = document.querySelectorAll('.column')[1];
 let advertising = document.querySelector('.adv');
+
+let fiveMenu = document.createElement('li');
+fiveMenu.className = 'menu-item';
+fiveMenu.textContent = 'Пятый элемент';
+menu.appendChild(fiveMenu);
 
 menu.insertBefore(li3, li2);
 document.body.style.background = 'url(img/apple_true.jpg) center no-repeat';
 title.textContent = 'Мы продаем только подлинную технику Apple';
-column.removeChild(advertising);
+document.querySelectorAll('.column')[1].removeChild(advertising);
 getAttitudes(getQuestionUser());
 
 function getQuestionUser() {
@@ -19,3 +23,8 @@ function getAttitudes(a) {
     let idPrompt = document.querySelector('.prompt');
     idPrompt.textContent = 'Ваше отношение к технике Apple: ' + a;
 }
+
+let adv = document.querySelector('.adv');
+let col = document.querySelector('.column');
+
+col[1].removeChild(adv);
