b33fb3a92ac29102bb9ea8ce19dd5a28a5747204
Roman <evofint@gmail.com>
2018-08-28 01:36:40 +0700

    ls9 easy fix

--- lesson 9/easy/js/script.js
+++ lesson 9/easy/js/script.js
@@ -141,11 +141,21 @@ window.addEventListener('DOMContentLoaded', () => {
     let more = document.querySelector('.more');
     let overlay = document.querySelector('.overlay');
     let close = document.querySelector('.popup-close');
+    let infoMore = document.querySelector('.info')
 
     more.addEventListener('click', function() {
         this.classList.add('more-splash');
         overlay.style.display = 'block';
         document.body.style.overflow = 'hidden';
+    });
+
+    infoMore.addEventListener('click', function() {
+        let target = event.target;
+        if(target.className == 'description-btn') {
+            this.classList.add('more-splash');
+            overlay.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        }
     });
 
     close.addEventListener('click', function () {
