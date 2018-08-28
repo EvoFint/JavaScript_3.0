c48dfe6488c9122ed33ed91f552f6b9b40d48f18
Roman <evofint@gmail.com>
2018-08-29 04:15:37 +0700

    ls10 easy fix

--- lesson 10/easy/script.js
+++ lesson 10/easy/script.js
@@ -9,16 +9,12 @@ class Options {
 
     showDiv() {
         let divBox = document.createElement('div');
+        divBox.style.cssText = `height: ${this.height}px; width: ${this.width}px; background: ${this.bg}; font-size: ${this.fontSize}px; text-aligin: ${this.textAlign};`
         divBox.textContent = 'Новый DIV';
-        divBox.style.height = this.height + 'px';
-        divBox.style.width = this.width + 'px';
-        divBox.style.background = this.bg;
-        divBox.style.fontSize = this.fontSize + 'px';
-        divBox.style.textAlign = this.textAlign;
         document.body.appendChild(divBox);
     }
 }
 
-const textDiv = new Options(600, 500, 'red', 30, 'centre');
+const textDiv = new Options(600, 500, 'red', 40, 'centre');
 
 textDiv.showDiv();
