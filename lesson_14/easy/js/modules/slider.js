5a887e6fbc77f62a31b02a5bd89da69b0295bbde
Roman <evofint@gmail.com>
2018-09-04 03:53:44 +0700

    ls14 fix

--- lesson_14/easy/js/modules/slider.js
+++ lesson_14/easy/js/modules/slider.js
@@ -13,10 +13,10 @@ function slider() {
     function showSlides(n) {
         if (n > slides.length) {
             slideIndex = 1;
-        };
+        }
         if (n < 1) {
             slideIndex = slides.length;
-        };
+        }
 
         for (var i = 0; i < slides.length; i++) {
             slides[i].style.display = 'none';
